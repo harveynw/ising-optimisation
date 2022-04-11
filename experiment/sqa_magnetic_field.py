8ce4ec67e44bdd3a56d332d6eb73e3f2ae070ea1
harveynw <harveywilliams@live.com>
2022-04-11 18:10:36 +0100

    Investigation

--- experiment/sqa_magnetic_field.py
+++ experiment/sqa_magnetic_field.py
@@ -1,7 +1,9 @@
 from pathlib import Path
 from typing import List
-from experiment.execute import execute_experiments, get_experiment_path, ExperimentResult
+from experiment.execute import execute_experiments, ExperimentResult
+from sim_anneal.plot import plot_energy
 from sim_quantum_anneal.hamiltonian import HamiltonianSQA
+from sim_quantum_anneal.plot import plot_energy_trotter_range
 from sim_quantum_anneal.problems import hamming_weight_spike
 from sim_quantum_anneal.quantum_anneal import QuantumAnneal
 
@@ -21,7 +23,7 @@ if __name__ == '__main__':
             'N': N,
             'P': 20,
             'T': T, 'T_pre': 3.0, 'T_n_steps': 50,
-            'gamma_start': 1.5, 'gamma_end': 1e-8, 'gamma_n_steps': 20
+            'gamma_start': 1.5, 'gamma_end': 1e-8, 'gamma_n_steps': 100
         }
 
         # Different values to test
@@ -36,7 +38,6 @@ if __name__ == '__main__':
     # Load it from file system and analyse
     results: List[ExperimentResult] = ExperimentResult.load_all(group_name=name, optimiser_name='QuantumAnneal')
 
-    energies_n = []
     for i, result in enumerate(results):
         print(f'Experiment {i}: {result.arguments}')
         energy_result = [hamming_weight_spike(r[1]) for r in result.result]
@@ -45,4 +46,14 @@ if __name__ == '__main__':
         print('Average', sum(energy_result)/len(energy_result))
         print('Max', max(energy_result))
 
-    print(energies_n)
+    # Analysing a particular result
+    exp_6 = results[6]
+    solved_run = exp_6.result[1]
+
+    final_energy, min_state, pre_history, sqa_history = solved_run
+
+    plot_energy(energy_func=hamming_weight_spike, history=pre_history, method="Pre-Anneal")
+    plot_energy_trotter_range(energy_func=hamming_weight_spike, history=sqa_history,
+                              method="Simulated Quantum Annealing")
+
+
